ce3f629ddfcca0e562dd7399dac2437017a05839
Ikruusha-Aadam <ikramaadam28@gmail.com>
2026-06-03 21:02:49 +0800

    summitted execise 4

---
+++ Exercise 4/Normal fuction/Expretion fuction/fuction 2.js
@@ -0,0 +1,10 @@
+
+const add= function(x,y){
+    return x+y;
+}
+console.log(add(22,33));
+console.log(add(26,43));
+console.log(add(28,44));
+console.log(add(66,99));
+console.log(add(20,30));
+console.log(add(12,13));
---
+++ Exercise 4/Normal fuction/Normal Fuction/fuction1.js
@@ -0,0 +1,11 @@
+
+function add(x){
+    return x;
+}
+console.log(add(3,4));
+console.log(add(30,40));
+console.log(add(32,43));
+console.log(add(33,45));
+console.log(add(37,47));
+
+
