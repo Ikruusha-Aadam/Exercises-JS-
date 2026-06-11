a7421ff019dafc7aecb46f60b6ba3245a5e7470f
Ikruusha-Aadam <ikramaadam28@gmail.com>
2026-06-11 08:39:21 +0800

    summited Exercise 7

---
+++ Exercise 7/Array Method(push,,pop) .js
@@ -0,0 +1,19 @@
+
+let colors =["Red","Blue","White"];
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+colors.push("Grean")
+console.log("after push");
+console.log(colors);
+
+console.log(colors.length);
+
+colors.shift();
+console.log("after shif");
+console.log(colors);
+
+console.log(colors.length);
+
