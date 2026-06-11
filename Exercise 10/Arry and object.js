13c5156830078eb7c62b61bbb387a142f90c9c1e
Ikruusha-Aadam <ikramaadam28@gmail.com>
2026-06-11 08:49:03 +0800

    Summited Execise 10

---
+++ Exercise 10/Arry and object.js
@@ -0,0 +1,22 @@
+
+let library=[
+    {
+        title:"Rich Dad Poor Dad",
+        autor: "Robert Kiyosaki",
+        year:2012
+    },
+    {
+        title:"The Alchemist",
+        autor: "Paulo Coelho",
+        year:2018
+    },
+     {
+        title:"Atomic Habits",
+        autor: "James Clear",
+        year:2017
+    }
+]
+
+console.log(library[0].title);
+console.log(library[1].autor);
+
