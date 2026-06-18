e5cb2177c96557e368dd2f304fd83dab0b518058
Ikruusha-Aadam <ikramaadam28@gmail.com>
2026-06-18 07:32:39 +0300

    Summited Exercise 17

---
+++ Exercise 17/else if .js
@@ -0,0 +1,15 @@
+
+let tempreture=15;
+    if( tempreture < 0)
+    {
+        console.log("Very cold")
+    }
+    else if(tempreture <=15){
+        console.log("Cold")
+    }
+    else if(tempreture<=25){
+        console.log("Warm")
+    }
+    else{
+        console.log("Hot")
+    }
