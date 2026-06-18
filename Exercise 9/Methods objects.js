b7d931b84835a950e3d04d8a5e6202289834404a
Ikruusha-Aadam <ikramaadam28@gmail.com>
2026-06-18 07:28:27 +0300

    Summited Exercise 9

---
+++ Exercise 9/Methods objects.js
@@ -0,0 +1,16 @@
+
+
+
+
+let car={
+    make:"China",
+    model:"Toyota",
+    year:1920,
+       start:function(){
+        console.log("The car has Started")
+       }
+     
+};
+ car.start()
+
+
